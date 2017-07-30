2c6a196c2b61a8143e7ae8fe6d30568df82994c3
Andy Evans <andrewevans0@outlook.com>
2017-07-30 08:05:09 +0100

    changing main controller parameters

--- src/main/java/rewind/MainController.java
+++ src/main/java/rewind/MainController.java
@@ -15,14 +15,14 @@ public class MainController {
     private DirectionRepository directionRepository;
 
     @GetMapping(path="/add") // Map ONLY GET Requests
-    public @ResponseBody String addNewUser (@RequestParam String name
-            , @RequestParam String email) {
+    public @ResponseBody String addNewUser (@RequestParam String time
+            , @RequestParam String dir) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
 
         Direction direction = new Direction();
-        direction.setTime(name);
-        direction.setDirection(email);
+        direction.setTime(time);
+        direction.setDirection(dir);
         directionRepository.save(direction);
         return "Saved";
     }
